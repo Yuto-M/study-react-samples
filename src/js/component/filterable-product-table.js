0a5417ea10c5d5f92a6eb67ab66d13bfa20cc02d
Yuto <miyakaway7@gmail.com>
2019-03-20 00:34:29 +0900

    reactの流儀step2までやった

---
+++ src/js/component/filterable-product-table.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "./search-bar";
+import ProductTable from "./product-table";
+
+export default class FilterableProductTable extends React.Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <ProductTable products={this.props.products} />
+            </div>
+        );
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Markdown from './component/markdown';
 import TemplatureConversion from './component/templature-conversion';
+import FilterableProductTable from "./component/filterable-product-table";
 
 
 if (document.getElementById('markdown')) {
@@ -15,5 +16,51 @@ if (document.getElementById('templature-conversion')) {
     ReactDOM.render(
         <TemplatureConversion />,
         document.getElementById('templature-conversion')
+    );
+}
+
+if (document.getElementById('filterable-product-table')) {
+    const PRODUCTS = [
+      {
+        category: "Sporting Goods",
+        price: "$49.99",
+        stocked: true,
+        name: "Football"
+      },
+      {
+        category: "Sporting Goods",
+        price: "$9.99",
+        stocked: true,
+        name: "Baseball"
+      },
+      {
+        category: "Sporting Goods",
+        price: "$29.99",
+        stocked: false,
+        name: "Basketball"
+      },
+      {
+        category: "Electronics",
+        price: "$99.99",
+        stocked: true,
+        name: "iPod Touch"
+      },
+      {
+        category: "Electronics",
+        price: "$399.99",
+        stocked: false,
+        name: "iPhone 5"
+      },
+      {
+        category: "Electronics",
+        price: "$199.99",
+        stocked: true,
+        name: "Nexus 7"
+      }
+    ];
+
+    ReactDOM.render(
+        <FilterableProductTable products={PRODUCTS} />,
+        document.getElementById('filterable-product-table')
     );
 }
