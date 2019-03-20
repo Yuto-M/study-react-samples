23fab9ce64bf8363759d41ece7f8a0e0ddd01ca4
miyakawa_yuto <miyakawa@inter-wave.jp>
2019-03-20 18:43:47 +0900

    step4完了

--- src/js/component/filterable-product-table.js
+++ src/js/component/filterable-product-table.js
@@ -3,11 +3,26 @@ import SearchBar from "./search-bar";
 import ProductTable from "./product-table";
 
 export default class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false,
+        };
+    }
+
     render() {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products} />
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
             </div>
         );
     }
--- src/js/component/search-bar.js
+++ src/js/component/search-bar.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 export default class SearchBar extends React.Component {
-  render() {
-    return (
-      <form>
-        <input type="text" placeholder="Search..." />
-        <p>
-          <input type="checkbox" /> Only show products in stock
-        </p>
-      </form>
-    );
-  }
+    render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
+        return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={filterText}
+                />
+                <p>
+                    <input
+                      type="checkbox"
+                      checked={inStockOnly}
+                    />
+                    {' '}
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
 }
